f083bc28a76e716a9248fdf5d78a114254504fa8
Thecreativecoder <mojisufi06@gmail.com>
2022-12-12 17:50:51 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Entry
  * Result : Always 0
  */
-int main(void) /*return value is an integer*/
+int main(void) /*return value is an integer and does nit tale arguments*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
