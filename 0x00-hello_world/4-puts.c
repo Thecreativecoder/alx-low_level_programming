7fe6cbab1d2daf4cc4e026692a39c5e9effda39e
Thecreativecoder <mojisufi06@gmail.com>
2022-12-12 17:16:57 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry
  * Result : Always 0
  */
-int main (void) /*return value is an integer*/
+int main(void) /*return value is an integer*/
 {
-	puts("\"Programming is like solving mutilingual puzzle");
+	puts("\"Programming is like building a mutilingual puzzle");
 	return (0);
 }
