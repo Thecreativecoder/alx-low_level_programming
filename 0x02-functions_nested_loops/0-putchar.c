4219bf4311ff56f35572d7163b991471b3b0aa16
Thecreativecoder <mojisufi06@gmail.com>
2022-12-14 18:28:07 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ *main - print "putchar" follow by a new line
+ *Description : you are not allowed to use standard libraries
+ *Return : 0
+ */
+int main(void)
+{
+	char alx[] = "_putchar"
+	int i = 0
+
+	while (alx(i) != '\n')
+	{
+		_putchar(alx[]);
+		i++
+	}
+	_putchar('\n')
+	return (0);
+}
