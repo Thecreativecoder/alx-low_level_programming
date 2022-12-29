451d0919cda94c4596930c41cd0cf1add8be7c81
Thecreativecoder <mojisufi06@gmail.com>
2022-12-29 09:24:28 -0800

    task 11 & 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of two two digits number
+ *        ranging from 0-99 seperaed by a comma, followed by a space
+ *
+ * Return: Always 0(sucess)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
