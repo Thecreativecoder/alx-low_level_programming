ec8021836a3771ed6003b19b17d18b644f5c30c7
Thecreativecoder <mojisufi06@gmail.com>
2023-01-01 13:54:16 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
- 
-}
+#include <stdio.h>
+
+/**
+ * main - print a sentence followed by a new line to standard error
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+
+	return (1);
+}	
