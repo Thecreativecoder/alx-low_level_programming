e26cd5f17c719106126a5346f374df4b9aa91315
Thecreativecoder <mojisufi06@gmail.com>
2023-01-02 07:46:26 -0800

    directory on pointers

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * -isalha - checks if a character is alphabetic.
- *  @c: the character to be checked 
+ * _isalha - checks if a character is alphabetic.
+ * @c: the character to be checked 
  *
  *  Return: 1 if character is letter , lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 { 
 	if ((c >= 'a' && c <= 'z') ||
-		       	(c >= 'A' && c <= 'Z'))
+			(c >= 'A' && <= 'Z'))
 
 		return (1);
 	else
 		return (0);
+}
